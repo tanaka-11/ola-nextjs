1a746667497644d1a18ed1c57c0bbd567caf0e5d
Marina Tanaka <mtanakasg@gmail.com>
2024-01-16 11:56:53 -0300

    criação de paginas

---
+++ src/pages/equipe.jsx
@@ -0,0 +1,15 @@
+import Menu from "@/components/Menu";
+
+export default function Equipe() {
+  return (
+    <>
+      <h1>@_Tanaka11_ 🎭</h1>
+      <Menu />
+
+      <main>
+        <h2>Equipe</h2>
+        <p>nois mesmo</p>
+      </main>
+    </>
+  );
+}
---
+++ src/pages/servicos.jsx
@@ -0,0 +1,19 @@
+import Menu from "@/components/Menu";
+
+export default function Servicos() {
+  return (
+    <>
+      <h1>@_Tanaka11_ 🎭</h1>
+      <Menu />
+
+      <main>
+        <h2>Serviços</h2>
+        <ol>
+          <li>Programa</li>
+          <li>Artes</li>
+          <li>Grau</li>
+        </ol>
+      </main>
+    </>
+  );
+}
